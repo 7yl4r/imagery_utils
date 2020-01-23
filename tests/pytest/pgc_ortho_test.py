025034b3cd37488342f2dba0e97a841a1060f068
7yl4r <murray.tylar@gmail.com>
2020-01-23 15:07:56 -0500

    try using another file

--- tests/pytest/pgc_ortho_test.py
+++ tests/pytest/pgc_ortho_test.py
@@ -12,23 +12,32 @@ import pytest
 class Test_pgc_ortho(TestCase):
     test_output_path = './tests/output'
 
-    def set_up(self):
+    def setUp(self):
         os.makedirs(os.path.dirname(self.test_output_path), exist_ok=True)
+
+    # def tearDown(self):
+    #     shutil.rmtree(os.path.dirname(self.test_output_path))
 
     @pytest.mark.testdata_required
     @patch('os.stat')  # patch to collect info instead of actually calling
     def test_pgc_doesnt_raise(self, mock_os_stat):
+        """
+        Functional test of pgc_ortho CLI usage.
+
+        pgc_ortho.py -p 4326 -c ns -t UInt16 -f GTiff ./tests/testdata/ortho/WV02_20131005052802_10300100278D8500_13OCT05052802-P1BS-500099283010_01_P004.NTF ./tests/tesdata/output
+        """
         from pgc_ortho import main
         import sys
         testargs = [
+            'pgc_ortho.py'
             '-p', '4326',
             '-c', 'ns',
             '-t', 'UInt16',
             '-f', 'GTiff',
             (
-                './tests/testdata/ortho/tests/testdata/ortho'
-                '/GE01_20110307105821_1050410001518E00_11MAR07105821-M1BS'
-                '-500657359080_01_P008.ntf'
+                './tests/testdata/ortho'
+                '/WV02_20131005052802_10300100278D8500_13OCT05052802-P1BS-'
+                '500099283010_01_P004.NTF'
             ),
             self.test_output_path
         ]
